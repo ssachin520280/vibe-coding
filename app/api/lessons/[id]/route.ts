739bb8a553290dc8635650b8ae3f7ff9052c5087
Sachin Anand <ssachin520280@outlook.com>
2025-05-19 08:00:33 +0530

    bugfix and add readme

--- app/api/lessons/[id]/route.ts
+++ app/api/lessons/[id]/route.ts
@@ -1,15 +1,17 @@
 import { connectToDB } from '@/lib/db';
 import Lesson from '@/lib/models/lesson';
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: Request, { params }: { params: Promise<{ id: string }>  }) {
   await connectToDB();
   const data = await req.json();
-  const lesson = await Lesson.findByIdAndUpdate(params.id, data, { new: true });
+  const { id } = await params;
+  const lesson = await Lesson.findByIdAndUpdate(id, data, { new: true });
   return Response.json(lesson);
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: Request, { params }: { params: Promise<{ id: string }>  }) {
   await connectToDB();
-  await Lesson.findByIdAndDelete(params.id);
+  const { id } = await params;
+  await Lesson.findByIdAndDelete(id);
   return Response.json({ ok: true });
 }
